9bce2366de07b12a0b86fb62a4758e3c8dc74d13
mishraanshuman786 <mishraanshuman146@gmail.com>
2026-04-29 09:28:57 +0530

    icon file changed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,16 @@
 import { initializeApp, getApps, getApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 
+
+console.log("ENV:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY)
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 }
+
+
 
 // Prevent multiple initializations (Next.js hot reload)
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
